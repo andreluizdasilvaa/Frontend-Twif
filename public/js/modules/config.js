7411b0829867acfc0e486cd70d094fb780381cc6
André <sandreluiz486@gmail.com>
2025-04-25 00:41:36 -0300

    add files from the responsiveness-and-pagination branch

--- public/js/modules/config.js
+++ public/js/modules/config.js
@@ -1,6 +1,5 @@
 const CONFIG = {
-    // sem "/" no final da URL
-    URL_API: "http://localhost:3000"
+    "URL_API": "http://localhost:3000"
 };
 
 export default CONFIG;
--- public/js/modules/feed/submitPost.js
+++ public/js/modules/feed/submitPost.js
@@ -19,7 +19,7 @@ export default async function submitPost() {
         // Sanitizando o conteúdo do post
         const conteudoSanitizado = DOMPurify.sanitize(conteudo);
 
-        fetch(`${CONFIG.URL_API}/create/post`, {
+        fetch(`${CONFIG.URL_API}/feed`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
