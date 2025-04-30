cd97304c9b5afc5230dfc1b13b0332e528a481a5
Júlia Rodrigues <167921624+juiaia@users.noreply.github.com>
2025-04-30 16:01:27 -0300

    API de suporte retorna problemas!
    a página fix-problem está retornando os problemas cadastrados pelo usuário na pag suporte, falta add CATEGORIA

--- public/js/fix-problem.js
+++ public/js/fix-problem.js
@@ -1,5 +1,9 @@
 import allFunctionFixProblem from "./modules/fix-problem/allFunctionFixProblem.js";
 
+import darkModeRelatorio from "./modules/ui/darkModeRelatorio.js";
+
 document.addEventListener('DOMContentLoaded', () => {
     allFunctionFixProblem();
+    darkModeRelatorio();
 });
+
