af568e31b6f8775245aca358a37720be55b0af72
André Luiz <154485939+andreluizdasilvaa@users.noreply.github.com>
2025-06-21 17:16:20 -0300

    Adiciona suporte a tooltip, melhorias no layout e funcionalidade de deletar e marcar como visualizado em relatórios; implementa lógica de feedback visual e persistência de estado nos botões.

--- public/js/modules/fix-problem/allFunctionFixProblem.js
+++ public/js/modules/fix-problem/allFunctionFixProblem.js
@@ -1,5 +1,6 @@
 import CONFIG from "../config.js";
 import verifyErrorsApi from "../utils/verifyErrorsApi.js";
+import { toastError, toastSuccess } from "../ui/Toast.js"; 
 
 // Função para formatar a data
 function formatarData(dataISO) {
@@ -25,7 +26,7 @@ export default function exibirProblemas() {
     })
     .then(data => {
       if (!data.length) {
-        container.innerHTML = "<p>Nenhum problema encontrado.</p>";
+        container.innerHTML = "<p id='text_order-not-found'>Nenhum problema encontrado...</p>";
         return;
       }
 
@@ -35,19 +36,81 @@ export default function exibirProblemas() {
 
         card.innerHTML = `
           <div class="header-card">
-            <div>
+            <div class="header-card-content">
               <strong>Nome: <span id="span-color-g">${pedido.nome}</span></strong>
+
+              <div class="tooltip">
+                <div class="icon">i</div>
+                <div class="tooltiptext">O usuário que enviou esse report, ira saber quer vc visualizou clicando no botão abaixo.</div>
+              </div>
+
             </div>
           </div>
           <blockquote class="mensagem">“${pedido.problema}”</blockquote>
           <footer class="data-report">
             Report enviado no dia ${formatarData(pedido.createdAt)}
-            <button class="btn-responder" title="Responder">
-              Marcar como visualizado
-              <img src="../assets/img/replyemail.png" alt="Responder" />
-            </button>
+
+            <div class="container-buttons-post">
+              <button class="btn" id="btn-delete-report">
+                <svg viewBox="0 0 15 17.5" height="17.5" width="15" xmlns="http://www.w3.org/2000/svg" class="icon">
+                  <path transform="translate(-2.5 -1.25)" d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z" id="Fill"></path>
+                </svg>
+              </button>
+
+              <button class="btn-responder" title="Responder" id="btn-mark-report">
+                Marcar como visualizado
+                <img src="../assets/img/replyemail.png" alt="Responder" />
+              </button>
+            </div>
+            
           </footer> 
         `;
+
+        // Adiciona eventListener para o botão de deletar
+        card.querySelector('#btn-delete-report').addEventListener('click', async () => {
+          try {
+            const response = await fetch(`${CONFIG.URL_API}/suport/delete`, {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ orderId: pedido.id }),
+              credentials: 'include',
+            });
+            if (response.ok) {
+              card.remove();
+              toastSuccess('Deletado com sucesso!');
+            } else {
+              toastError('Erro ao deletar o report.');
+            }
+          } catch (err) {
+            toastError('Erro ao deletar o report.');
+          }
+        });
+
+        // Adiciona eventListener para o botão de marcar como visualizado
+        card.querySelector('#btn-mark-report').addEventListener('click', async () => {
+          try {
+            const response = await fetch(`${CONFIG.URL_API}/suport/mark/viewed`, {
+              method: 'PATCH',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ orderId: pedido.id }),
+              credentials: 'include',
+            });
+            if (response.ok) {
+              card.querySelector('#btn-mark-report').textContent = 'Visualizado';
+              card.querySelector('#btn-mark-report').disabled = true;
+              card.querySelector('#btn-delete-report').style.display = 'none'
+              toastSuccess('Visualizado com sucesso!');
+            } else {
+              toastError('Erro ao marcar como visualizado.');
+            }
+          } catch (err) {
+            toastError('Erro ao marcar como visualizado.');
+          }
+        });
 
         container.appendChild(card);
       });
--- public/js/modules/suporte/suporte.js
+++ public/js/modules/suporte/suporte.js
@@ -14,16 +14,23 @@ import { toastInfo, toastError, toastSuccess } from "../ui/Toast.js";
 })();
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('button').addEventListener('click', async (event) => {
+    const btn = document.querySelector('button');
+    btn.addEventListener('click', async (event) => {
         event.preventDefault();  // Evita o comportamento padrão de envio de formulário
+        btn.disabled = true;
+        btn.style.opacity = '0.6';
         const description = document.getElementById('input-texto').value;
 
         // Verifique se todos os campos foram preenchidos
         if (!description) {
             toastInfo('Escreva alguma coisa antes de enviar!');
+            btn.disabled = false;
+            btn.style.opacity = '';
             return;
         } else if(description.length > 190) {
             toastInfo('Maximo de 190 caracteres!');
+            btn.disabled = false;
+            btn.style.opacity = '';
             return;
         }
 
@@ -40,13 +47,19 @@ document.addEventListener('DOMContentLoaded', () => {
             const result = await response.json();
             if (response.ok) {
                 toastSuccess(result.msg);
-                window.location.href = "/";
+                setTimeout(() => {
+                    window.location.href = "/feed";
+                }, 3000);
             } else {
                 toastError(result.msg || 'Ocorreu um erro, tente novamente mais tarde.');
+                btn.disabled = false;
+                btn.style.opacity = '';
             }
         } catch (error) {
             console.error('Erro ao enviar os dados:', error);
             toastError('Erro ao enviar os dados. Tente novamente mais tarde.');
+            btn.disabled = false;
+            btn.style.opacity = '';
         }
     });
 });
